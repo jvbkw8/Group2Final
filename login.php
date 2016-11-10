231bc350475817a6dd873d32a0039b0a6ea8edaa
Zaphster <Zaphster@users.noreply.github.com>
2016-11-10 15:58:58 -0600

    Update login.php

--- login.php
+++ login.php
@@ -1,10 +1,3 @@
-<?php
-
-	session_start(); // session starts with the help of this function
-
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -20,81 +13,53 @@
 
     <title>RADs Login</title>
 
-   
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-    
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" rel="stylesheet">
-    
-    <!--<link href="../bootstrap/assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">-->
-
-    
-    <!--<link href="app.css" rel="stylesheet">-->
-
-    
-    <!--<script src="../bootstrap/assets/js/ie-emulation-modes-warning.js"></script>-->
-		<script>
-
-
-		</script>
   </head>
 
   <body>
-  <?php //include "header.php"; 
-        include "connection.php";
-  ?>
-	<div class="container">
-		<?php
-            $user = $_POST['username'];
-            $user_password = $_POST['password'];
-            $action = '';
-            $link = mysqli_connect("$servername", "$username", "$password", "$dbname") or die ("Connection Error " . mysqli_error($link));
-            //TODO:  hash password!
-		$sql = "SELECT username FROM user WHERE username = '$user' AND password= '$user_password';";
-            $result = mysqli_query($link, $sql);
-            $row = mysqli_fetch_assoc($result);
-
-            if (isset($_POST['submit']) && !empty($user) 
-               && !empty($user_password)) {
-				
-               if ($user_password == $row['password'] ) {
-		       echo "hey you logged in successfully";
-		$_SESSION[NAME] = $row['username'];
-                header("location: index.html");
-                 
-               }else {
-                  echo 'Wrong username or password';
-               }
-            }
-         ?>
-		<div class="row">
-			<div class="col-md-4 col-sm-4 col-xs-3"></div>
-			<div class="col-md-4 col-sm-4 col-xs-6">
-				<h2>Login</h2>
-				<form action="<?=$_SERVER['PHP_SELF']?>" method="POST">
-					<div class="row form-group">
-							<input class='form-control' type="text" name="username" placeholder="username">
-					</div>
-					<div class="row form-group">
-							<input class='form-control' type="password" name="password" placeholder="password">
-					</div>
-					<div class="row form-group">
-							<input class=" btn btn-info" type="submit" name="submit" value="Login"/>
-					</div>
-				</form>
-
-			</div>
+	<div class="row">
+		<div class="col-md-4 col-sm-4 col-xs-3"></div>
+		<div class="col-md-4 col-sm-4 col-xs-6">
+			<h2>Login</h2>
+			<form onsubmit="verifyLogin();" method="POST">
+				<div class="row form-group">
+						<input class='form-control' type="text" name="username" placeholder="username">
+				</div>
+				<div class="row form-group">
+						<input class='form-control' type="password" name="password" placeholder="password">
+				</div>
+				<div class="row form-group">
+						<input class=" btn btn-info" type="submit" name="submit" value="Login"/>
+				</div>
+			</form>
 
 		</div>
 
-
-
-
+	</div>
 		
 	<!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <!--<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>-->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	  <script>
+		  function verifyLogin(){
+			$.ajax(
+				url: "verifyLogin.php",
+				success: function(html){
+					if(html.success == true){
+						window.location.replace("index.php");	
+					} else {
+						alert(html);
+					}
+				},
+				error: function(html){
+					console.log("error: " + html);
+				}
+			);	  
+		  }
+	  </script>
   </body>
 </html>
