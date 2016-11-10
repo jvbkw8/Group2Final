8518155b43b62142ac187b8d5d0d39555303aebf
Zaphster <Zaphster@users.noreply.github.com>
2016-11-10 17:06:23 -0600

    Update login.php

--- login.php
+++ login.php
@@ -37,7 +37,10 @@
 		</div>
 
 	</div>
-	  <div id="error"><?php=$error?>
+	  <div id="error">
+		  <?php
+		  echo $_GET['error'];
+		  ?>
 	  </div>
 		
 	<!-- Bootstrap core JavaScript
@@ -46,28 +49,5 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <!--<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>-->
 	  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	  <script>
-// 		  function verifyLogin(){
-// 			console.log("attempting to verify login");
-// 			var username = $('#user').val();
-// 			var userpassword = $('#password').val();
-// 			$.ajax({
-// 				url: "loginVerify.php",
-// 				data: {username:username, userpassword:userpassword},
-// 				method: "POST",
-// 				success: function(html){
-// 					console.log(html);
-// 					if(html.success == true){
-// 						window.location.replace("index.html");	
-// 					} else {
-// 						$('#error').html("Invalid username or password.");
-// 					}
-// 				},
-// 				error: function(html){
-// 					console.log("error: " + html);
-// 				}
-// 			});	  
-// 		  }
-	  </script>
   </body>
 </html>
