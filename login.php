d41d5b657370c83fc96764de6f5ace5705aff057
Zaphster <Zaphster@users.noreply.github.com>
2016-11-10 17:09:55 -0600

    Update login.php

--- login.php
+++ login.php
@@ -50,4 +50,11 @@
     <!--<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>-->
 	  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   </body>
+	<script>
+		$(document).ready(function(){
+			if($("#error").html() == ""){
+				$("#error").toggle();
+			}
+		});
+	</script>
 </html>
