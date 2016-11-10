1a0dcc6e4ac529e415b4e32d4df948d98fd3bf39
Zaphster <Zaphster@users.noreply.github.com>
2016-11-10 16:14:29 -0600

    Update loginVerify.php

--- loginVerify.php
+++ loginVerify.php
@@ -1,95 +1,23 @@
 <?php
+	$failure = "{success:false}";
+	$success = "{success:true}";
+        $username = $_POST['username'];
+        $userpassword = $_POST['userpassword'];
+	if(!$username or !$userpassword){
+		echo $failure;
+	}
 	session_start(); // session starts with the help of this function
-?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-   
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-	
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="shortcut icon" href="favicon.ico">
-
-    <title>RADs Login</title>
-
-   
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-    
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" rel="stylesheet">
-    
-    <!--<link href="../bootstrap/assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">-->
-
-    
-    <!--<link href="app.css" rel="stylesheet">-->
-
-    
-    <!--<script src="../bootstrap/assets/js/ie-emulation-modes-warning.js"></script>-->
-		<script>
-		</script>
-  </head>
-
-  <body>
-  <?php //include "header.php"; 
         include "connection.php";
-  ?>
-	<div class="container">
-		<?php
-            $user = $_POST['username'];
-            $user_password = $_POST['password'];
-            $action = '';
-            $link = mysqli_connect("$servername", "$username", "$password", "$dbname") or die ("Connection Error " . mysqli_error($link));
-            //TODO:  hash password!
-		$sql = "SELECT username FROM user WHERE username = '$user' AND password= '$user_password';";
-            $result = mysqli_query($link, $sql);
-            $row = mysqli_fetch_assoc($result);
-            if (isset($_POST['submit']) && !empty($user) 
-               && !empty($user_password)) {
-				
-               if ($user_password == $row['password'] ) {
-		       echo "hey you logged in successfully";
+	$conn = new mysqli($servername, $username, $password);
+	//$link = mysqli_connect("$servername", "$username", "$password", "$dbname") or die ("Connection Error " . mysqli_error($link));
+	//TODO:  hash password!
+	$sql = "SELECT username FROM db.user WHERE username = '$user' AND password= '$user_password';";
+	$result = $conn->query($sql);
+	$row = $result->fetch_assoc();
+	if ($username == $row['username'] ) {
 		$_SESSION[NAME] = $row['username'];
-                header("location: index.html");
-                 
-               }else {
-                  echo 'Wrong username or password';
-               }
-            }
-         ?>
-		<div class="row">
-			<div class="col-md-4 col-sm-4 col-xs-3"></div>
-			<div class="col-md-4 col-sm-4 col-xs-6">
-				<h2>Login</h2>
-				<form action="<?=$_SERVER['PHP_SELF']?>" method="POST">
-					<div class="row form-group">
-							<input class='form-control' type="text" name="username" placeholder="username">
-					</div>
-					<div class="row form-group">
-							<input class='form-control' type="password" name="password" placeholder="password">
-					</div>
-					<div class="row form-group">
-							<input class=" btn btn-info" type="submit" name="submit" value="Login"/>
-					</div>
-				</form>
-
-			</div>
-
-		</div>
-
-
-
-
-		
-	<!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <!--<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>-->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  </body>
-</html>
+		echo $success;
+	}else {
+		echo $failure;
+	}
+?>
