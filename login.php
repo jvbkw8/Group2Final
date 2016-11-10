fdd84247d8d5b70ae06cb7791ee528262d6d5cbc
Zaphster <Zaphster@users.noreply.github.com>
2016-11-10 16:26:10 -0600

    Update login.php

--- login.php
+++ login.php
@@ -45,7 +45,8 @@
     <!--<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>-->
 	  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	  <script>
-		  function verifyLogin(){
+		  function verifyLogin(evt){
+			evt.preventDefault();
 			console.log("attempting to verify login");
 			var username = $('#user').val();
 			var userpassword = $('#password').val();
