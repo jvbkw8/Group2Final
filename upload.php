bd5ea95317371d95b43d48d18cd7fc40b83c15e8
Jared <jvbkw8@mail.missouri.edu>
2016-11-10 12:21:48 -0600

    upload

--- upload.php
+++ upload.php
@@ -1,1 +1,31 @@
-upload form
+<!DOCTYPE html>
+<html>
+<body>
+	<div class="form-group row">
+		<label for=example-first-input" class="col-xs-2 col-form-label">First Name:</label>
+		<div class="col-xs-10">
+			<input class="form-control" type="text" placeholder="First Name" id="firstname">
+	</div>
+	<div class="form-group row">
+		<label for="example-last-input" class="col-xs-2 col-form-label">Last Name:</label>
+		<div class="col-xs-10">
+			<input clas="form-control" type="text" placeholder="Last Name" id="lastname">
+	</div>
+	<div class="form-group row">
+  		<label for="example-date-input" class="col-xs-2 col-form-label">Date</label>
+ 		 <div class="col-xs-10">
+			 <input class="form-control" type="date" value="2011-08-19" id="example-date-input">
+ 	 </div>	
+	
+
+
+
+
+<input type="radio" name="user" value="student">Student<br>
+		<input type="radio" name="user" value="researcher">Researcher<br>
+		<input type="radio" name="user" value="datascientist">Data Scientist<br>
+		Date: 
+		<input type="text" name="date">
+	
+</body>
+</html>
