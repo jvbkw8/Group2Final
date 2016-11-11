0281b7cc42b2f61400ac6ba310c62a165ca0b2b8
nihr43 <nihr43@mail.missouri.edu>
2016-11-10 21:01:54 -0600

    Create accountInfo.php

---
+++ accountInfo.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>RADs(Research Analysis and Database for Scientists)</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+<div class="container">
+  <h2>Welcome to RADs</h2>
+  <p>Research Analysis and Database for Scientists</p>
+  <ul class="nav nav-pills nav-justified">
+    <li><a href="index.html">Home</a></li>
+    <li><a href="upload.html">Upload</a></li>
+    <li><a href="#">Search Manifests</a></li>
+    <li class="active"><a href="#">Account</a></li>
+    <li><a href="#">Logout</a></li>
+  </ul>
+  <br>
+  <h1> Firstname: </h1>
+  <h1> Lastname: </h1>
+  <h1> username: </h1>
+</div>
+
+</body>
+</html>
