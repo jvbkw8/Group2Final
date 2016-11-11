fce5471906220de67bf8db53e6f31beae331fda3
Zaphster <Zaphster@users.noreply.github.com>
2016-11-10 20:51:39 -0600

    Update signupVerify.php

--- signupVerify.php
+++ signupVerify.php
@@ -14,7 +14,8 @@
     $conn = new mysqli($servername, $username, $password);
     $hashedPassword = password_hash($user_password);
     $sql = "INSERT into (username, hashedpassword) values ($user_name, $hashedPassword);";
-    $conn->query($sql) or header("Location: signup.php?error=Connection error");
+    header("Location: signup.php?error=$sql");exit();
+    $conn->query($sql) or {header("Location: signup.php?error=Connection error");exit();}
     if($conn->affected_rows != 1){
       header("Location: signup.php?error=Information not stored");
         exit();
