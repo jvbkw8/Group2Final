0d5a60232163f399e6a6834f63fe1f29ea0634bd
Zaphster <Zaphster@users.noreply.github.com>
2016-11-15 14:24:31 -0600

    Update header.php

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
   <h2>Upload Manifest</h2>
   <br>
   <ul class="nav nav-pills nav-justified">
-    <li><a href="index.html">Home</a></li>
+    <li><a href="index.php">Home</a></li>
     <li class="active"><a href="upload.php">Upload</a></li>
     <li><a href="search.php">Search Manifests</a></li>
     <li><a href="account.php">Account</a></li>
