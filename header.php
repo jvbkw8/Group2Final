805ff9c4d7ed62b2d5ec68bfffb95039ec8b253b
Zaphster <Zaphster@users.noreply.github.com>
2016-11-15 14:15:19 -0600

    Create header.php

---
+++ header.php
@@ -0,0 +1,11 @@
+<div class="container">
+  <h2>Upload Manifest</h2>
+  <br>
+  <ul class="nav nav-pills nav-justified">
+    <li><a href="index.html">Home</a></li>
+    <li class="active"><a href="upload.php">Upload</a></li>
+    <li><a href="search.php">Search Manifests</a></li>
+    <li><a href="account.php">Account</a></li>
+    <li><a href="logout.php">Logout</a></li>
+  </ul>
+</div>
