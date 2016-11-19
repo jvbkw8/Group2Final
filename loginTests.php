0e1561a44b973cc60d90b448bd4792e8efc7bf1b
Zaphster <Zaphster@users.noreply.github.com>
2016-11-18 19:25:01 -0600

    Create loginTests.php

---
+++ loginTests.php
@@ -0,0 +1,31 @@
+<?php
+include "security.php";
+if(!$_SESSION['ADMIN']){
+  header("Location: login.php");
+}
+?>
+<script>
+  var loginCredentials = [
+    {test:true, username:"", userpassword:"",expected:false}, //no username or password
+    {test:true, username:"", userpassword:"test", expected:false},//no username
+    {test:true, username:"testing", userpassword: "invalidpassword", expected:false}, //valid username, invalid password
+    {test:true, username:"testing", userpassword: "test", expected:true}, //valid username and password
+    {test:true, username:"tesTIng", userpassword: "test", expected:false}, //username with capitals changed, password that would work for username: testing
+    {test:true, username:"testing", userpassword: "teSt", expected:false}, //valid username, password with capitals changed
+    {test:true, username:"admin", userpassword: "admin", expected:true}, //another valid username and password
+    {test:true, username:"admin;delete from user where username = 'testing';": "admin", expected:false}, //sql injection!!!
+    {test:true, username:"testing", userpassword: "test", expected:true} //test if the known account is still there after the sql injection attempt
+  ]
+  for(var i = 0; i < loginCredentials.length; i++){
+    $.ajax({
+      url:"loginVerify.php",
+      data: loginCredentials[i],
+      success: function(html){
+        $('#results').append($('<div>').html("success: " + html.success + "   username: " + html.name + "   isadmin: " + html.isadmin + "   dberrors: " + html.error));
+      },
+      error:
+    });
+  }
+</script>
+<div id="results">
+</div>
