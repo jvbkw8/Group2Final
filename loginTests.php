436e2f241a9ce6d61558df791cca8be6efae2fd4
Zaphster <Zaphster@users.noreply.github.com>
2016-11-18 20:27:19 -0600

    Update loginTests.php

--- loginTests.php
+++ loginTests.php
@@ -45,6 +45,10 @@
     });
   }
 </script>
+<head> 
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
 <table id="results">
   <thead>
     <th>Test Passed</th>
