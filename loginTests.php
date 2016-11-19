30e18028b4f76443b4a0eacd422b4b551f1734d7
Zaphster <Zaphster@users.noreply.github.com>
2016-11-18 19:27:32 -0600

    Update loginTests.php

--- loginTests.php
+++ loginTests.php
@@ -13,7 +13,7 @@ if(!$_SESSION['ADMIN']){
     {test:true, username:"tesTIng", userpassword: "test", expected:false}, //username with capitals changed, password that would work for username: testing
     {test:true, username:"testing", userpassword: "teSt", expected:false}, //valid username, password with capitals changed
     {test:true, username:"admin", userpassword: "admin", expected:true}, //another valid username and password
-    {test:true, username:"admin;delete from user where username = 'testing';": "admin", expected:false}, //sql injection!!!
+    {test:true, username:"admin;delete from user where username = 'testing';", "admin", expected:false}, //sql injection!!!
     {test:true, username:"testing", userpassword: "test", expected:true} //test if the known account is still there after the sql injection attempt
   ]
   for(var i = 0; i < loginCredentials.length; i++){
