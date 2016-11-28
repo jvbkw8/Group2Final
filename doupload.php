0db3268bcc6ccecc8095ad64dbb6da349e393d51
nihr43 <nihr43@mail.missouri.edu>
2016-11-28 16:44:12 -0600

    fixed redirect

--- doupload.php
+++ doupload.php
@@ -49,5 +49,5 @@ if(isset($_POST) and $_SERVER['REQUEST_METHOD'] == "POST"){
 
 $stmt->close();
 $conn->close();
-header( 'Location: /upload.php' ) ;
+header( 'Location: /Group2Final/upload.php' ) ;
 ?>
