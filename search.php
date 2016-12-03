7e90bcb70066fccebcf6e8f7c68ce8b331990085
nihr43 <nihr43@mail.missouri.edu>
2016-12-03 10:49:28 -0600

    Update search.php

--- search.php
+++ search.php
@@ -68,7 +68,7 @@ tr:nth-child(even) {
 			<div class="col-md-6 col-sm-10 col-xs-10">
 				<h2></h2>
 				<form action="/Group2Final/search.php" method="post">				
-					  <label for="example-first-input" class=" col-form-label" width="40">By filename:</label>			 
+					  <label for="example-first-input" class=" col-form-label" width="40px">By filename:</label>			 
 					  <br>
 					  <input class="form-control" type="text" placeholder="" name="searchname">
 					  <br>
