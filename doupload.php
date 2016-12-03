068f369706185eaf9c879d37a36228bd042e76db
Zaphster <Zaphster@users.noreply.github.com>
2016-12-03 16:29:10 -0600

    Update doupload.php

--- doupload.php
+++ doupload.php
@@ -27,6 +27,16 @@ if(isset($_POST) and $_SERVER['REQUEST_METHOD'] == "POST"){
 	//set manifest name for all
 	$manifestname = $_POST["manifestname"];
 	
+	$stmt = $conn->prepare("SELECT name FROM manifest where name = ?");
+	$stmt->bind_param("s", $manifestname);
+	if(!$stmt->execute()){
+		$error[] = "Problem when checking if manifest name exists";
+	}
+	$mcheck = "";
+	$stmt->bind_result($mcheck);
+	while($stmt->fetch()){
+		$error[] = "Manifest name ".$mcheck." already exists.";
+	}
 	//insert manifest name and get its id
 	$stmt = $conn->prepare("INSERT INTO manifest(name) VALUES (?)");
 	$stmt->bind_param("s", $manifestname);	
@@ -36,38 +46,40 @@ if(isset($_POST) and $_SERVER['REQUEST_METHOD'] == "POST"){
 	}
 	$manifestid = mysqli_insert_id($conn);
 	
-	// for each file
-	foreach ($_FILES['files']['name'] as $f => $name) {
-	//echo $name;
-	//echo "<br>";
-	//continue;
-		
-	    if ($_FILES['files']['error'][$f] == 4) {
-	        continue; // Skip file if any error found
-	    }	       
-	    if ($_FILES['files']['error'][$f] == 0) {	           
-	        if ($_FILES['files']['size'][$f] > $max_file_size) {
-	            $error[] = "$name is too large!.";
-	            continue; // Skip large files
-	        }
+	if(count($error) == 0){
+		// for each file
+		foreach ($_FILES['files']['name'] as $f => $name) {
+		//echo $name;
+		//echo "<br>";
+		//continue;
 
-	        else{ // No error found
-			//for each file do these things
-			$binaryData = file_get_contents($_FILES['files']['tmp_name'][$f]);
-			$owner = $_SESSION['NAME'];
-			$null = NULL; //this made it all work
-			
-			// prepare and bind
-			$stmt = $conn->prepare("INSERT INTO files (data, name, owner, manifestid) VALUES (?, ?, ?, ?)");
-			$stmt->bind_param("bssi", $null,  $name, $owner, $manifestid);
-			$stmt->send_long_data(0, $binaryData);	//this made it all work	
-			if(!$stmt->execute()){
-				$numFilesNotUploaded++;
-			} else {
-				$numFilesUploaded++;
+		    if ($_FILES['files']['error'][$f] == 4) {
+			continue; // Skip file if any error found
+		    }	       
+		    if ($_FILES['files']['error'][$f] == 0) {	           
+			if ($_FILES['files']['size'][$f] > $max_file_size) {
+			    $error[] = "$name is too large!";
+			    continue; // Skip large files
 			}
-	        }
-	    }
+
+			else{ // No error found
+				//for each file do these things
+				$binaryData = file_get_contents($_FILES['files']['tmp_name'][$f]);
+				$owner = $_SESSION['NAME'];
+				$null = NULL; //this made it all work
+
+				// prepare and bind
+				$stmt = $conn->prepare("INSERT INTO files (data, name, owner, manifestid) VALUES (?, ?, ?, ?)");
+				$stmt->bind_param("bssi", $null,  $name, $owner, $manifestid);
+				$stmt->send_long_data(0, $binaryData);	//this made it all work	
+				if(!$stmt->execute()){
+					$numFilesNotUploaded++;
+				} else {
+					$numFilesUploaded++;
+				}
+			}
+		    }
+		}
 	}
 } else {
 	$error[] = "No files uploaded.";
