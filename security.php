45fcae621554d906309dc3dcc78a7e94942ad786
Zaphster <Zaphster@users.noreply.github.com>
2016-12-03 18:00:30 -0600

    Update security.php

--- security.php
+++ security.php
@@ -1,6 +1,19 @@
 <?php
 session_start();
+$requireLogin = false;
 if(!isset($_SESSION['NAME'])){
+  $requireLogin = true;
+} else {
+  include "connection.php";
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  $query = "SELECT * from user where username = ".$_SESSION['NAME']." and activeuserflag = 1";
+  $r = mysqli_query($conn, $query);
+  if(!$r->num_rows){
+     $requireLogin = true;
+  }
+  
+}
+if($requireLogin){
   header("Location: /Group2Final/login.php");
 }
 ?>
